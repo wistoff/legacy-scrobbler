1693e8621fb6ffef812625a0573b8aaad2d70d2b
Kjell Wistoff <kjell@wistoff.de>
2026-04-29 10:01:34 +0200

    architecture handling in forge configuration

--- forge.config.js
+++ forge.config.js
@@ -1,15 +1,7 @@
 // build keys when signing and notarizing the app
 // const buildKeys = require('./build-scripts/build-keys')
 
-const os = require('os')
 const package = require('./package.json')
-
-// Get command line arguments
-const args = process.argv.slice(2) // Exclude first two elements
-
-// Extract architecture argument if present
-const archIndex = args.indexOf('--arch')
-const arch = archIndex !== -1 ? args[archIndex + 1] : os.arch()
 
 module.exports = {
   packagerConfig: {
@@ -38,19 +30,21 @@ module.exports = {
     // Make Windows installer
     {
       name: '@electron-forge/maker-squirrel',
-      config: {
+      config: (arch) => ({
         authors: 'Kjell Wistoff',
         description:
           'Legacy Scrobbler enables users to sync their listening history from offline devices to their Last.fm profile, preserving their music legacy in the digital era.',
         setupExe: `Legacy Scrobbler-${package.version}-setup-${arch}.exe`,
         setupIcon: './images/icon.ico',
         loadingGif: './images/win-setup-loading.gif'
-      }
+      })
     },
     // Make DMG file for macOS
     {
       name: '@electron-forge/maker-dmg',
-      config: {
+      config: (arch) => ({
+        name: `Legacy Scrobbler-${package.version}-${arch}`,
+        title: `Legacy Scrobbler ${arch}`,
         background: './images/dmg-background-gatekeeper.png',
         format: 'ULFO',
         icon: './images/icon.icns',
@@ -63,7 +57,7 @@ module.exports = {
             path: `${process.cwd()}/out/Legacy Scrobbler-darwin-${arch}/Legacy Scrobbler.app`
           }
         ]
-      }
+      })
     },
     // {
     //   name: '@electron-forge/maker-zip',
